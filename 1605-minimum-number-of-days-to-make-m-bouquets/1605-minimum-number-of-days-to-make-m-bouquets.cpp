4ad1a234f7fcf4a85da3ff57f867c0c1377bc962
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-06-15 21:22:12 +0530

    Time: 64 ms (98.73%), Space: 129.9 MB (5.37%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -3,23 +3,23 @@ public:
 bool possible(vector<int> arr, int day , int m , int k, int n){
         int cnt = 0;
         int boq = 0;
-    for(int i = 0; i<n; i++){
-        if(arr[i] <= day){
+    for (int i = 0; i < n; i++) {
+        if (arr[i] <= day) {
             cnt++;
         }
-        else{
+        else {
             boq += cnt/k;
             cnt = 0;
         }
     }
     boq += cnt/k;
-    if( boq >= m) return true;
+    if (boq >= m) return true;
     else return false;
 }
 int min(vector<int> arr, int n){
     int mini = arr[0];
-    for(int i = 0;i<n;i++){
-        if(arr[i] < mini){
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < mini) {
             mini = arr[i];
         }
     }
@@ -28,8 +28,8 @@ int min(vector<int> arr, int n){
 
 int max(vector<int> arr, int n){
     int max = arr[0];
-    for(int i = 0;i<n; i++){
-        if(arr[i] > max){
+    for (int i = 0; i < n; i++) {
+        if(arr[i] > max) {
             max = arr[i];
         }
     }
@@ -41,8 +41,8 @@ int max(vector<int> arr, int n){
         int n = bloomDay.size();
         int low = min(bloomDay,n), high = max(bloomDay,n);
         int ans = -1;
-        while(low <= high){
-            int mid = (low+high) / 2;
+        while (low <= high) { 
+            int mid = (low + high) / 2;
             if(possible(bloomDay,mid, m ,k,n)){
                 ans = mid;
                 high = mid - 1;
