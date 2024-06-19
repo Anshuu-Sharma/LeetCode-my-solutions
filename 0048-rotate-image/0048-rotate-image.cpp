ac2d96e43a881c5e4d93aab95648cf1552f2d448
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-06-19 14:47:05 +0530

    Time: 0 ms (100%), Space: 8.7 MB (60.93%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         int n = matrix.size();
         int m = matrix[0].size();
 
