43456121cb99d72ff82975cd32dd66b3c0df43d1
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-06-19 13:58:43 +0530

    Time: 12 ms (38.81%), Space: 16.6 MB (39.55%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    void markrow(vector<vector<int>>& matrix,int row, int m) {
+        for(int j = 0; j<m; j++) {
+            if(matrix[row][j] != 0) {
+                matrix[row][j] = -1111;
+            }
+        }
+    }
+
+    void markcol(vector<vector<int>>& matrix,int col, int n){
+        for(int j = 0; j<n; j++) {
+            if(matrix[j][col] != 0) {
+                matrix[j][col] = -1111;
+            }
+        }
+    }
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        bool ahead = false;
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<m; j++) {
+                if(matrix[i][j] == 0){
+                    markrow(matrix,i,m);
+                    markcol(matrix,j,n);
+                    ahead = true;
+                }
+            }
+        }
+        
+        if(ahead == true){
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<m; j++) {
+                if(matrix[i][j] == -1111) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
+};
