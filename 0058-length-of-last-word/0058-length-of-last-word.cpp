a7bf1595db6827e1f89167f8db93e7514a979625
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-06-20 22:38:23 +0530

    Time: 3 ms (46.46%), Space: 7.6 MB (86.62%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         int cnt = 0;
         int n = s.size();
         int a = n-1;
-        while(s[a] == ' ') {
-            a--;
-        }
-        for(int i = a; i>=0; i--) {
-                if(s[i] != ' ') cnt++;
+        while( s[a] == ' ') a--;
 
-            else if(s[i] == ' ') return cnt;
+        for (int i = a; i >= 0; i--) {
+                if (s[i] != ' ') cnt++;
+
+            else if (s[i] == ' ') return cnt;
 
             
         }
