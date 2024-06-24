96aa8cea3a13b7351791292c31cc3443a48f7a81
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-06-24 22:04:21 +0530

    Time: 22 ms (90.86%), Space: 15.3 MB (90.69%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
@@ -4,24 +4,10 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
-        stack<char> stk;
-        for(int i = 0; i<num.size(); i++) {
-            stk.push(num[i]);
+        while(num.length()){
+            if((num.back()-'0')%2==0) num.pop_back();
+            else break;
         }
-
-        if(stk.top() % 2 != 0) return num;
-        string ans = "";    
-        while(!stk.empty()) {
-            if((stk.top() % 2) == 0) stk.pop();
-            else {
-                while(!stk.empty()) {
-                    ans.insert(0,1,stk.top());
-                    stk.pop();
-                }
-            }
-        }
-
-        
-        return ans;
+        return num;
     }
 };
