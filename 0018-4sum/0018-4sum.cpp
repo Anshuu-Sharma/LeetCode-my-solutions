9480ff6cc072713764a16b702824228c301576b7
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-06-30 22:00:21 +0530

    Time: 29 ms (49.23%), Space: 16 MB (50.85%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int n = nums.size();      
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
