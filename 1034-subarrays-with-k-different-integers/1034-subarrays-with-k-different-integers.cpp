4549abee07a2b81147bfc9c12d55442d665b0839
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-07-03 20:24:37 +0530

    Time: 106 ms (84.68%), Space: 55.8 MB (70.32%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+Solution() {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+}
     int f(vector<int> & nums, int k) {
         int l = 0 , r = 0, cnt = 0;
         unordered_map<int, int> mpp;
