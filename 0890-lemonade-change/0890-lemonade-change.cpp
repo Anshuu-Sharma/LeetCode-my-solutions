57583b60e6e61b0dbeeadd6dfc2afe50832777a9
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-07-04 21:02:58 +0530

    Time: 58 ms (95.39%), Space: 85.9 MB (19.68%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
         int five = 0, ten = 0;
         for(int i = 0; i<bills.size(); i++) {
             if(bills[i] == 5) five++;
