22659dfb3483b60dd2d300281a047d6e27c3b55a
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-07-07 23:08:07 +0530

    Time: 4 ms (90.35%), Space: 8.5 MB (62.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         if(s.size() != t.size()) return false;
         vector<int> hash(26,0);
         for(int i = 0; i<s.size(); i++) {
