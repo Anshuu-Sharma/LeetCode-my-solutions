b5fdcf15281bc1dc752d3448c34bc8cbe667d1b0
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-07-09 19:05:24 +0530

    Time: 2004 ms (9.08%), Space: 509.6 MB (13.19%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,3 +1,10 @@
+const int ZERO = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
 class Solution {
 public:
     static bool comp(vector<int> val1, vector<int> val2) {
