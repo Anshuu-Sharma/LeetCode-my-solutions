8343e9db44ed30aa9967de9f9914c54ce0e94ced
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-07-11 12:56:29 +0530

    Time: 9 ms (50.82%), Space: 21.8 MB (12.74%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -12,6 +12,9 @@
 class Solution {
 public:
     int depthcheck(TreeNode* root) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         if(root == NULL) return 0;
 
         int lh = depthcheck(root->left);
