ab9d7f5841fd1e1b5332722a22e90113f8e1e51d
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-07-14 12:11:46 +0530

    Time: 3 ms (94.02%), Space: 18.6 MB (5.44%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -17,21 +17,24 @@ public:
         int ans = 0;
         queue<pair<TreeNode*,  unsigned long long>> q;
         q.push({root, 0});
-        while(!q.empty()) {
+        while( !q.empty() ) {
             unsigned long long size = q.size();
             unsigned long long mmin = q.front().second;
             unsigned long long first = 0, last = 0;
-            for(int i = 0; i<size; i++) {
+            for( int i = 0; i < size; i++ ) 
+            {
                 unsigned long long cur_id = q.front().second - mmin;
                 TreeNode* temp = q.front().first;
                 q.pop();
-                if(i == 0) first = cur_id;
-                if(i == size-1) last = cur_id;
+                if( i == 0 ) first = cur_id;
+                if( i == size-1 ) last = cur_id;
                 
-                if(temp->left) {
+                if( temp->left ) 
+                {
                     q.push({temp->left, 2 * cur_id + 1});
                 }
-                if(temp->right) {
+                if( temp->right )
+                {
                     q.push({temp->right, 2 * cur_id + 2});
                 }
             }
