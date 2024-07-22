5400285380dbacf4dc529611c13b93216eb2b879
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-07-22 15:35:23 +0530

    Time: 0 ms (100%), Space: 9.2 MB (10.3%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int myAtoi(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         int i = 0;
         int n = s.size();
         
