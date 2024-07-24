93f55a6dfd6e4fabbdad1e7ae2238c30c6533f8b
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-07-24 11:25:20 +0530

    Time: 9 ms (87.9%), Space: 20.7 MB (36.67%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,14 +2,25 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-        int n = nums.size();
-        int sum = ((n*n) + n) / 2;
-        int actualsum =0;
-        for ( auto it : nums) {
-            actualsum +=it;
-        }
+        cin.tie(NULL);
+        // MTD 1
+        // int n = nums.size();
+        // int sum = ((n*n) + n) / 2;
+        // int actualsum =0;
+        // for ( auto it : nums) {
+        //     actualsum +=it;
+        // }
+        // return (sum - actualsum);
 
-        return (sum - actualsum);
+        // MTD 2
+        int xor1 = 0;
+        int xor2 = 0;
+         for(int i = 0; i<nums.size(); i++)
+        {
+            xor1 = xor1^nums[i];
+            xor2 = xor2^i;
+        }
+        xor2 = xor2^nums.size();
+        return xor1^xor2; 
     }
 };
