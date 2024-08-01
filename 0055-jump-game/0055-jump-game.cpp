c7897d22130b9314b9d3db3db8fcc0db7e39a605
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-08-01 23:03:40 +0530

    Time: 27 ms (98.83%), Space: 51.1 MB (41.12%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
         int maxlen = 0;
         for(int i = 0; i<nums.size(); i++) {
             if(i > maxlen) return false;
