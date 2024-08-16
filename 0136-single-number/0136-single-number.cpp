1f116aee4eace15215766444deafb7c54a85d7c2
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-08-16 22:03:41 +0530

    Time: 4 ms (97.87%), Space: 19.8 MB (26.21%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
+        cin.tie(nullptr);
+   
         int xor1 = 0;
         for(auto it : nums) {
             xor1 ^= it;
