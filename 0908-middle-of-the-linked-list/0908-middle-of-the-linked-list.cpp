5c684321a4493096e310eaf6a124bbd3fde60228
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-08-24 22:57:43 +0530

    Time: 0 ms (100%), Space: 9.6 MB (14.18%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -11,6 +11,9 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         ListNode* slow = head;
         ListNode* fast = head;
 
