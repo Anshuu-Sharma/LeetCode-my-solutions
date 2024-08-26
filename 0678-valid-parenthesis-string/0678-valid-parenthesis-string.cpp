0867d523fb5a481b29193f99e8ce9ae7d8026353
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-08-26 22:01:32 +0530

    Time: 0 ms (100%), Space: 7.7 MB (28.55%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool checkValidString(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         int min = 0, max = 0;
         for (int i = 0; i < s.size(); i++) {
             if (s[i] == '(') {
