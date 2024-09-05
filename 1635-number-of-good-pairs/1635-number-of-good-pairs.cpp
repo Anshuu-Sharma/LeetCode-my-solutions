8232974dc1f8964a4b232ee7d40eaebdcac44e4d
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-09-05 14:40:40 +0530

    Time: 0 ms (100%), Space: 9 MB (40.9%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
+        ios::sync_with_stdio(false); cin.tie(nullptr);
         int cnt = 0;
 
         for(int i = 0; i < nums.size()-1; i++)
