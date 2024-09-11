33d08dbafd46e1e90ddd9b0d6c88a8c479da2323
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-09-11 22:03:18 +0530

    Time: 10 ms (95.73%), Space: 27.2 MB (39.76%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         int cnt = 0;
         int el;
         for(int i = 0;i<nums.size();i++){
