690257eaf741d90f73ecdc26de5ddbd5ce66e5d5
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-09-12 22:16:49 +0530

    Time: 2 ms (69.88%), Space: 9.5 MB (15.48%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     string removeOuterParentheses(string S) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         string res = "";
         int cnt = 0, start = 0, n = S.size();
         for (int i = 0; i < n; ++i) {
