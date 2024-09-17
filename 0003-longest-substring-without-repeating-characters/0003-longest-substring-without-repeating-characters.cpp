77318b2bd1f000a5a8512546acd9a17bbf0649f8
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-09-17 22:00:55 +0530

    Time: 8 ms (78.06%), Space: 11.3 MB (74.29%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         vector<int> hash(256,-1);
         int l = 0, r = 0;int n = s.size();
         int maxlen = 0;int len = 0;
