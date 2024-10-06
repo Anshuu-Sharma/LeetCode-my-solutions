309ca4b1dd2e2e3bfc1e20605c6eb71023351f5c
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-10-06 22:08:49 +0530

    Time: 0 ms (100%), Space: 14.1 MB (86.91%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -21,6 +21,10 @@ public:
         
     }
     vector<int> rightSideView(TreeNode* root) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+
         vector<int> res;
         recursion(root, 0, res);
         return res;
