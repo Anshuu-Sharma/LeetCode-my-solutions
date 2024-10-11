894ffcf4f25731a37a42a6752b6a4c9e9a98c982
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-10-11 16:19:41 +0530

    Time: 254 ms (82.42%), Space: 132.7 MB (95.57%) - LeetHub

--- 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int minimumArea(vector<vector<int>>& grid) {
-        if (grid.empty() || grid[0].empty()) return 0; // Check for empty grid or row
-        
+        if (grid.empty() || grid[0].empty()) return 0;
         int rowCount = grid.size();
         int colCount = grid[0].size();
         int firstRow = rowCount, lastRow = -1;
         int firstCol = colCount, lastCol = -1;
         
-        // Traverse the grid to find the first and last rows and columns containing 1s
+       
         for (int i = 0; i < rowCount; i++) {
             for (int j = 0; j < colCount; j++) {
                 if (grid[i][j] == 1) {
@@ -20,10 +19,10 @@ public:
             }
         }
         
-        // If no '1' was found, return 0
+    
         if (lastRow == -1 || lastCol == -1) return 0;
         
-        // The area is determined by the rectangular bounds containing all 1s
+       
         return (lastRow - firstRow + 1) * (lastCol - firstCol + 1);
     }
 };
