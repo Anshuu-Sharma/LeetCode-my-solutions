2a5c127290f7f7acef1cedc47386a6b336d5de6b
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-10-15 22:43:35 +0530

    Time: 389 ms (5.01%), Space: 424.8 MB (5.88%) - LeetHub

--- 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
@@ -9,8 +9,13 @@ public:
         return temp;
     }
     int triangularSum(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         if (nums.size() == 1) return nums[0];
+
         vector<int> temp = adder(nums);
+
         while (temp.size() > 1) {
             temp = adder(temp);
         }
