9193b6ca4505ac82f06fa37444c06d03692116e3
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-10-16 22:00:38 +0530

    Time: 413 ms (54.47%), Space: 185 MB (87.53%) - LeetHub

---
+++ 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode* start = head;
+        ListNode* end = head;
+        ListNode* temp = head;
+
+        // First, find the length of the list `n`
+        int n = 0;
+        while (temp) {
+            n++;
+            temp = temp->next;
+        }
+
+        // Find the `k-th` node from the start
+        temp = head;
+        for (int i = 1; i < k; i++) {
+            temp = temp->next;
+        }
+        start = temp;  // `start` now points to the `k-th` node from the start
+            // it is important to keep this statement out of any loop to be executed;
+
+        // Find the `k-th` node from the end
+        temp = head;
+        for (int i = 1; i < (n - k + 1); i++) {
+            temp = temp->next;
+        }
+        end = temp;  // `end` now points to the `k-th` node from the end
+         // it is important to keep this statement out of any loop to be executed;
+
+        // Swap the values of `start` and `end`
+        int t = start->val;
+        start->val = end->val;
+        end->val = t;
+
+        return head;
+    }
+};
