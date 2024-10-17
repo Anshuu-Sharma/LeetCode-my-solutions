6eb654606c96386dc548a8b2e73a094c943487e8
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-10-17 12:55:41 +0530

    Time: 0 ms (100%), Space: 10.1 MB (43.57%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,21 +11,57 @@
  */
 class Solution {
 public:
-    void preorder(TreeNode* root, vector<int>& arr) {
-        if(root == NULL) return;
-
-        arr.push_back(root->val);
-        preorder(root->left, arr);
-        preorder(root->right, arr);
-    }
-   
-    vector<int> preorderTraversal(TreeNode* root) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
+    // ITERATIVE
+    vector<int> preorderTraversal(TreeNode* root){
+        vector<int> ans;
+        if(root == NULL) return ans;
         
-        vector<int> arr;
-        preorder(root, arr);
+        stack<TreeNode*> st;
+        st.push(root);
 
-        return arr;
+        while(!st.empty()){
+            TreeNode* temp = st.top();
+            st.pop();
+            if(temp->right) st.push(temp->right);
+            if(temp->left) st.push(temp->left);
+            ans.push_back(temp->val);
+        }
+        return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+    // void preorder(TreeNode* root, vector<int>& arr) {
+    //     if(root == NULL) return;
+
+    //     arr.push_back(root->val);
+    //     preorder(root->left, arr);
+    //     preorder(root->right, arr);
+    // }
+   
+    // vector<int> preorderTraversal(TreeNode* root) {
+    //     ios_base::sync_with_stdio(false);
+    //     cin.tie(nullptr);
+        
+    //     vector<int> arr;
+    //     preorder(root, arr);
+
+    //     return arr;
+    // }
 };
