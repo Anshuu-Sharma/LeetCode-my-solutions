ba31a3bd8531c410710cd3a171d053910b7bb145
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-10-17 15:10:17 +0530

    Time: 7 ms (10.66%), Space: 13.5 MB (18.59%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -44,4 +44,37 @@ public:
         }
         return ans;
     }
+    // VECTOR METHOD
+    // vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+    //     vector<vector<int>> result;
+    //     if(root==NULL){
+    //         return result;
+    //     }
+
+    //     queue<TreeNode*>nodesqueue;
+    //     nodesqueue.push(root);
+    //     bool lefttoright=true;
+
+    //     while(!nodesqueue.empty()){
+    //         int size=nodesqueue.size();
+    //         vector<int>row(size);
+    //         for(int i=0;i<size;i++){
+    //             TreeNode* node=nodesqueue.front();
+    //             nodesqueue.pop();
+                
+    //             int index=(lefttoright)?i:(size-1-i);
+    //             row[index]=node->val;
+    //             if(node->left){
+    //                 nodesqueue.push(node->left);
+    //             }
+    //             if(node->right){
+    //                 nodesqueue.push(node->right);
+    //             }
+    //         }
+    //         lefttoright=!lefttoright;
+    //         result.push_back(row);
+    //     }
+    //     return result;
+    // }
+
 };
