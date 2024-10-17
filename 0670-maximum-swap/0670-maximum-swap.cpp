34df0919217ee99144ef3c8d82e42ae7e81bbc58
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-10-17 18:48:49 +0530

    Time: 0 ms (100%), Space: 7.3 MB (98.12%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string numStr = to_string(num); 
+        int n = numStr.size();
+
+        vector<int> rightMaxIndex(10, -1); 
+
+
+        for (int i = 0; i < n; ++i) {
+            rightMaxIndex[numStr[i] - '0'] = i;
+        }
+
+
+        for (int i = 0; i < n; ++i) {
+
+            for (int d = 9; d > numStr[i] - '0'; --d) {
+                if (rightMaxIndex[d] > i) { 
+                    swap(numStr[i], numStr[rightMaxIndex[d]]);
+                    return stoi(numStr); 
+                }
+            }
+        }
+
+        return num;
+    }
+};
