da4ae7ea7d82fd4d4a3c21e3f2cd56df526874d5
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-10-18 16:33:06 +0530

    Time: 5 ms (96.84%), Space: 26.6 MB (5.41%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -30,6 +30,9 @@ public:
 
 
     int findBottomLeftValue(TreeNode* root) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         traverse(root, 0);
         return mpp[n-1];
     }
