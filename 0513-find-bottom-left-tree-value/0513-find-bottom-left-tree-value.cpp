b2f29f76864303a16f582c59aa5fa787d8284d10
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-10-18 16:27:01 +0530

    Time: 8 ms (91.6%), Space: 26.7 MB (5.41%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    map<int, int> mpp;
+    int n = 0;
+
+public:
+    void traverse(TreeNode* root, int level){
+        if(!root) return;
+
+        if(mpp.find(level) == mpp.end()){
+            mpp[level] = root->val;
+            n++;
+        }
+
+        traverse(root->left, level+1);
+        traverse(root->right, level+1);
+
+    }
+
+
+    int findBottomLeftValue(TreeNode* root) {
+        traverse(root, 0);
+        return mpp[n-1];
+    }
+};
