2ce2bf6e1e95434e7804212957a932bd834afaff
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-10-23 20:48:00 +0530

    Time: 18 ms (97.18%), Space: 43 MB (69.99%) - LeetHub

---
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int finder(vector<int>& arr, int low , int high){
+        int maxi = low;
+        for(int i = low; i <= high; i++){
+            if(arr[maxi] < arr[i]) maxi = i;
+        }
+        return maxi;
+    }
+
+    TreeNode* helper(vector<int>& arr, int low, int high){
+        if (low > high) return nullptr;  // Base case: when the range is invalid
+        
+        int divide = finder(arr, low, high);
+        TreeNode* root = new TreeNode(arr[divide]);
+
+        // Recursively construct the left and right subtrees
+        root->left = helper(arr, low, divide - 1);
+        root->right = helper(arr, divide + 1, high);
+
+        return root;
+    }
+
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        return helper(nums, 0, nums.size() - 1);
+    }
+};
