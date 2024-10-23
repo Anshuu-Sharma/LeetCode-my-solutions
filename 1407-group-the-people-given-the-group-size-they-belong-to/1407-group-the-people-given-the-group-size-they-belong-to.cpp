1fa67b68519e7a9e5665e83f975cdee48c75125f
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-10-23 14:02:15 +0530

    Time: 7 ms (73.29%), Space: 17 MB (34.51%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         vector<vector<int>> ans;
 
         vector<pair<int , int>> storage;
