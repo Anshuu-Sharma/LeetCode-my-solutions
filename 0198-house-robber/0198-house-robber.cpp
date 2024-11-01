68570894efd84c69436e5ecadfb7fae360255c99
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-11-01 20:22:27 +0530

    Time: 0 ms (100%), Space: 9.6 MB (45.12%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public: 
-    int helper(vector<int>& nums, int ind, vector<int>& dp){
-        if(ind == 0) return dp[ind] = nums[ind];
-        if(ind < 0) return 0;
 
-        if(dp[ind] != -1) return dp[ind];
-        int pick = nums[ind] + helper(nums, ind-2, dp);
-        int not_pick = 0 + helper(nums, ind - 1, dp);
-
-        return dp[ind] = max(pick, not_pick);
-    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1, -1);
+        vector<int> dp(n+1, 0);
 
-        return helper(nums, n-1, dp);
+        dp[0] = nums[0];
+
+        for(int i = 1; i<n; i++){
+            int take = nums[i];
+            if(i>1){
+                take += dp[i-2];
+            } 
+    
+            int not_take = 0 + dp[i-1];
+
+            dp[i] = max(take, not_take);
+        }
+        return dp[n-1];
     }
+
+    // MEMOIZATION METHOD
+    // int helper(vector<int>& nums, int ind, vector<int>& dp){
+    //     if(ind == 0) return dp[ind] = nums[ind];
+    //     if(ind < 0) return 0;
+
+    //     if(dp[ind] != -1) return dp[ind];
+    //     int pick = nums[ind] + helper(nums, ind-2, dp);
+    //     int not_pick = 0 + helper(nums, ind - 1, dp);
+
+    //     return dp[ind] = max(pick, not_pick);
+    // }
+    // int rob(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<int> dp(n+1, -1);
+
+    //     return helper(nums, n-1, dp);
+    // }
 
     // RECURSION METHOD
     // int helper(vector<int>& nums, int ind){
