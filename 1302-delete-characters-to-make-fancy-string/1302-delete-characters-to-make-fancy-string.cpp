fb80e008318350c8fb1ba9879091f11f6f51fdea
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-11-01 14:50:40 +0530

    Time: 11 ms (98.7%), Space: 36.4 MB (93.02%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        string ans = "";
-        for (char& c : s) {
-            int n = ans.size();
-            if (n < 2 || ans[n - 1] != c || ans[n - 2] != c) {
-                ans += c;
-            }
-        }
-        return ans;
+    if(s.length()<3) {
+        return s;
     }
+    int j = 2;
+    for (int i = 2; i < s.size(); ++i)
+        if (s[i] != s[j - 1] || s[i] != s[j - 2])
+            s[j++] = s[i];
+    s.resize(j);
+    return s;
+}
+    // string makeFancyString(string s) {
+    //     string ans = "";
+    //     for (char& c : s) {
+    //         int n = ans.size();
+    //         if (n < 2 || ans[n - 1] != c || ans[n - 2] != c) {
+    //             ans += c;
+    //         }
+    //     }
+    //     return ans;
+    // }
 };
