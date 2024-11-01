242b3cc9f145dbed6f944cc2e96a2e06f109b20f
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-11-01 20:13:15 +0530

    Time: 0 ms (100%), Space: 9.5 MB (45.12%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public: 
+    int helper(vector<int>& nums, int ind, vector<int>& dp){
+        if(ind == 0) return dp[ind] = nums[ind];
+        if(ind < 0) return 0;
+
+        if(dp[ind] != -1) return dp[ind];
+        int pick = nums[ind] + helper(nums, ind-2, dp);
+        int not_pick = 0 + helper(nums, ind - 1, dp);
+
+        return dp[ind] = max(pick, not_pick);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1, -1);
+
+        return helper(nums, n-1, dp);
+    }
+
+    // RECURSION METHOD
+    // int helper(vector<int>& nums, int ind){
+    //     if(ind == 0) return nums[ind];
+    //     if(ind < 0) return 0;
+
+    //     int pick = nums[ind] + helper(nums, ind-2);
+    //     int not_pick = 0 + helper(nums, ind - 1);
+
+    //     return max(pick, not_pick);
+    // }
+    // int rob(vector<int>& nums) {
+    //     int n = nums.size();
+
+    //     return helper(nums, n-1);
+    // }
+};
