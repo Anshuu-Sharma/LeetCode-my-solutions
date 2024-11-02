48061a15b2aca8bbf8eca26b819d7f1af3fdb0c0
Anshuu-Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-11-02 09:38:08 +0530

    Time: 0 ms (100%), Space: 8.1 MB (19.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -3,23 +3,23 @@ public:
 
     int uniquePaths(int m, int n){
         vector<vector<int>> dp(m, vector<int>(n, 0));
-
+        vector<int> prev(n, 0);
+        vector<int> curr(n, 0);
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
-                if(i == 0 && j == 0) dp[0][0] = 1;
+                if(i == 0 && j == 0) curr[0] = 1;
                 else{
                     int up = 0;
                     int left = 0;
 
-                    if(i>0) up = dp[i-1][j];
-                    if(j>0) left = dp[i][j-1];
-                    dp[i][j] = up + left;
+                    if(i>0) up = prev[j];
+                    if(j>0) left = curr[j-1];
+                    curr[j] = up + left;
                 }
-
             }
+            prev = curr;
         }
-        return dp[m-1][n-1];
-
+        return prev[n-1];
     }
     // MEMOIZATION METHOD
     // int dp_helper(int n, int m, vector<vector<int>>& dp){
