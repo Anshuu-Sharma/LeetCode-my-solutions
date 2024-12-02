0639ca0284f74c22854bba87578b81bbc7845647
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2024-12-02 07:57:03 +0530

    Time: 0 ms (100%), Space: 8.4 MB (6.42%) - LeetHub

---
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+//KNK
+    int isPrefixOfWord(string sentence, string searchWord) {
+        stringstream ss(sentence);
+        string s;
+        for (int i = 1; ss >> s; ++i) {
+            if (s.find(searchWord) == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
