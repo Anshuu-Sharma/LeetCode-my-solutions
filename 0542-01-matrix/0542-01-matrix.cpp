ddfacaf171a29173aba8c7f1fd8c1e067a70d04a
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2025-01-01 17:03:32 +0530

    Time: 24 ms (43.55%), Space: 38.8 MB (34.21%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,40 +3,41 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
-        vector<vector<int>> ans(m, vector<int>(n, 0));
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-
-        queue<pair<pair<int, int>, int>> q;
+        vector<vector<int>> vis(m, vector<int>(n,0));
+        vector<vector<int>> ans(m, vector<int>(n,0));
+        queue<pair<pair<int,int>,int>> q;
+        // adding intital 0s in queue
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
                 if(mat[i][j] == 0){
-                    q.push({{i,j},0});
+                    q.push({{i,j}, 0});
                     vis[i][j] = 1;
                 }
             }
         }
 
-        int drow[4] = {-1,0,1,0};
-        int dcol[4] = {0,1,0,-1};
-
+        //
+        int drow[] = {-1,0,1,0};
+        int dcol[] = {0,1,0,-1};
         while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int dist = q.front().second; //dist hai from nearest zero
-
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int dist = q.front().second;
             q.pop();
-            ans[row][col] = dist;
+            ans[r][c] = dist;
             for(int i = 0; i<4; i++){
-                int nrow = row + drow[i];
-                int ncol = col + dcol[i];
+                int nr = r + drow[i];
+                int nc = c + dcol[i];
 
-                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol] == 0){
-                    vis[nrow][ncol] = 1;
-                    q.push({{nrow, ncol}, dist + 1});
+                if(nr>=0 && nc>=0 && nr<m && nc<n && vis[nr][nc] == 0){
+                    vis[nr][nc] = 1;
+                    q.push({{nr,nc}, dist + 1});
                 }
-            }
-
+            
+            } 
         }
+
         return ans;
+
     }
 };
