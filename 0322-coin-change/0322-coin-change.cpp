bd0c69734e7cae48cfb33c70bcb3af2f0b69b4bd
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2025-03-15 16:58:25 +0530

    Time: 50 ms (25.84%), Space: 81.6 MB (5.08%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
-    int recursionHelper(vector<int>& coins, int amount, int ind, vector<vector<int>>& dp){
+    // int recursionHelper(vector<int>& coins, int amount, int ind, vector<vector<int>>& dp){
         
-        if(ind == 0){
-            if(amount % coins[ind] == 0) return dp[ind][amount] = amount/coins[ind];
-            else return dp[ind][amount] = 1e9;
-        }
+    //     if(ind == 0){
+    //         if(amount % coins[ind] == 0) return dp[ind][amount] = amount/coins[ind];
+    //         else return dp[ind][amount] = 1e9;
+    //     }
 
-        if(dp[ind][amount] != -1) return dp[ind][amount];
-        int notTake = 0 + recursionHelper(coins, amount, ind-1, dp);
-        int take = INT_MAX;
-        if(coins[ind] <= amount) take = 1 + recursionHelper(coins, amount-coins[ind], ind ,dp);
-        return dp[ind][amount] = min(take, notTake);
+    //     if(dp[ind][amount] != -1) return dp[ind][amount];
+    //     int notTake = 0 + recursionHelper(coins, amount, ind-1, dp);
+    //     int take = INT_MAX;
+    //     if(coins[ind] <= amount) take = 1 + recursionHelper(coins, amount-coins[ind], ind ,dp);
+    //     return dp[ind][amount] = min(take, notTake);
 
-    }
+    // }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         sort(coins.begin(),coins.end());
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int ans = recursionHelper(coins, amount, n-1, dp);
+        vector<vector<long long>> dp(n, vector<long long>(amount+1, -1));
+        // int ans = recursionHelper(coins, amount, n-1, dp);
+        // return ans>=1e9 ? -1 : ans;
+
+        for(int i = 0; i<amount+1; i++){
+            if(i % coins[0] == 0) dp[0][i] = i/coins[0];
+            else dp[0][i] = INT_MAX;
+        }
+
+        for(int i = 1; i<n; i++){
+            for(int j = 0; j<amount+1; j++){
+                // if(dp[ind][amount] != -1) return dp[ind][amount];
+                long long notTake = 0 + dp[i-1][j];
+                long long take = INT_MAX;
+                if(coins[i] <= j) take = 1 + dp[i][j-coins[i]];
+                dp[i][j] = min(take, notTake);
+            }
+        }
+        long long ans =  dp[n-1][amount];
         return ans>=1e9 ? -1 : ans;
     }
 };
