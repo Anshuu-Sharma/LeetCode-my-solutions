cf1d76b70c5b248efc5deabb44a973ee5dcdce57
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2025-03-31 12:32:32 +0530

    Time: 35 ms (39.91%), Space: 10.1 MB (31.58%) - LeetHub

--- 2031-egg-drop-with-2-eggs-and-n-floors/2031-egg-drop-with-2-eggs-and-n-floors.cpp
+++ 2031-egg-drop-with-2-eggs-and-n-floors/2031-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int twoEggDrop(int n) {
-        return ceil((-1 + sqrt(1 + 8 * n)) / 2);
+        vector<vector<int>> dp(3, vector<int>(n + 1, 0));
+
+        // Base case
+        for (int j = 1; j <= n; j++) {
+            dp[1][j] = j;
+        }
+
+        // Filling the table for 2 eggs
+        for (int j = 1; j <= n; j++) {
+            dp[2][j] = 1e9; 
+            for (int x = 1; x <= j; x++) {
+                dp[2][j] = min(dp[2][j], 1 + max(dp[1][x - 1], dp[2][j - x]));
+            }
+        }
+
+        return dp[2][n];
     }
 };
