fd04fc32622df6c6d209bda4cd9d3f657a77580f
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2025-03-31 09:26:05 +0530

    Time: 0 ms (100%), Space: 8.9 MB (17.77%) - LeetHub

--- 1761-count-sorted-vowel-strings/1761-count-sorted-vowel-strings.cpp
+++ 1761-count-sorted-vowel-strings/1761-count-sorted-vowel-strings.cpp
@@ -4,14 +4,14 @@ public:
         vector<vector<int>> dp(n+1, vector<int>(6,0));
 
         // Base case
-        for(int i = 1; i<=5; i++){
+        for (int i = 1; i <= 5; i++) {
             dp[1][i] = i;
         }
 
         // main loop for calulating
-        for(int i = 2; i<=n; i++){
-            for(int j = 1; j<=5; j++){
-                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= 5; j++) {
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
             }
         }
         return dp[n][5];
