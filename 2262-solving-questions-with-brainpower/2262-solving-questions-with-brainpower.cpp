6e9064641e0380e4f1b90816b23841ee16a10e5a
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2025-04-01 23:55:45 +0530

    Time: 0 ms (100%), Space: 119.7 MB (74.65%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    long long helper(vector<vector<int>>& questions, int i, int n, vector<long long>& dp){
-        if(i>=n) return 0;
-
-        if(dp[i] != -1) return dp[i];
-
-        long long notTake = 0 + helper(questions, i+1, n, dp);
-        long long take = questions[i][0] + helper(questions, i+1+questions[i][1], n, dp);
-
-        return dp[i] = max(take, notTake);
-    }
     long long mostPoints(vector<vector<int>>& questions) {
         int n = questions.size();
-        vector<long long> dp(n, -1);
-        return helper(questions, 0, n, dp);
+        vector<long long> dp(n+1, 0);  
+
+        for (int i = n - 1; i >= 0; i--) {
+            long long notTake = dp[i + 1];
+            long long take = questions[i][0];  
+            
+            int nextIdx = i + 1 + questions[i][1];  
+            if (nextIdx < n) {
+                take += dp[nextIdx];
+            }
+            
+            dp[i] = max(take, notTake);  
+        }
+
+        return dp[0];  
     }
 };
