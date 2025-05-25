3969cb6d9ddd44bf19c4aa861b2fc92bc890edf5
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2025-05-25 18:25:22 +0530

    Time: 0 ms (100%), Space: 8.7 MB (17.33%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-//     int helper(int n, vector<int>& dp){
-// if (n==0 || n==1) return 1;
+    // int helper(int n, vector<int>& dp) {
+    //     if(n==0 || n==1) return dp[n] = 1;
 
-//         if(dp[n]!= -1) return dp[n];
-//         int oneStep = helper(n-1, dp);
-//         int twoStep = helper(n-2,dp);
+    //     if(dp[n] != -1) return dp[n];
 
-//         return dp[n] = oneStep + twoStep;
-//     }
+    //     return climbStairs(n-1) + climbStairs(n-2);
+    // }
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1); 
-        dp[0]= 1; dp[1] = 1; 
-        for(int i = 2; i<=n; i++){
-             dp[i] = dp[i-1] + dp[i-2]; 
+        // if(n==0 || n==1) return 1;
+
+        // int oneStep = climbStairs(n-1);
+        // int twoStep = climbStairs(n-2);
+        // return oneStep+twoStep;
+
+        vector<int> dp(n+1,-1);
+        dp[0] = 1, dp[1] = 1;
+        for(int i = 2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        // return helper(n, dp);
         return dp[n];
     }
 };
