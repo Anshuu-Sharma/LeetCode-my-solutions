0489e6b73eaea964c4544aa9d17c02335864eb11
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2025-06-01 09:01:56 +0530

    Time: 31 ms (26.47%), Space: 9 MB (57.06%) - LeetHub

--- 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     long long distributeCandies(int n, int limit) {
         long long cnt = 0;
-        for (int x = 0; x <= limit; ++x) {
+        long long firstMin = max(0, n - limit - limit);
+        long long firstMax = min(limit, n);
+        for (int x = firstMin; x <= firstMax; ++x) {
             int minY = max(0, n - x - limit);
             int maxY = min(limit, n - x);
             if (minY <= maxY) {
