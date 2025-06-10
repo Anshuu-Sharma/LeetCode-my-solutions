5d6ded04734dfa789187d0ce4b88f334fb325e36
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2025-06-10 09:15:29 +0530

    Time: 7 ms (68.71%), Space: 14.9 MB (99.38%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -5,33 +5,27 @@ public:
         int n = nums.size();
         int cnt = 0;
 
-        if(n==1) return 0;
-
-        if(n == 2) {
-            int diff = abs(nums[0]-nums[1]);
-            return diff == k ? 1:0;
-        }
+        // if(n==1) return 0;
 
        int prev1 = 1e9;
        int prev2 = 1e9; 
-
 
         for(int i = 0; i<n-1; i++) {
             for(int j = i+1; j<n; j++) {
                 int diff = abs(nums[i] - nums[j]);
                 if(diff == k) {
+                    if(prev1 == nums[i]) break;
                     if(prev1 == 1e9 && prev2 == 1e9) {
                         prev1 = nums[i];
                         prev2 = nums[j];
                         cnt++;
                     }
                     else if(prev1 != 1e9 && prev2 != 1e9) {
-                        if(prev1 == nums[i]) break;
-                        else {
+                        
                             prev1 = nums[i];
                             prev2 = nums[j];
                             cnt++;
-                        } 
+                         
                     }
                 }
                 else if(diff > k) break;
