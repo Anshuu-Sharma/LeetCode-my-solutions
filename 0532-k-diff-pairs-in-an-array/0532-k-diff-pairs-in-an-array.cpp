19d5bd5404b8936d6da5f38b90319123fb57770a
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2025-06-10 09:10:42 +0530

    Time: 7 ms (68.71%), Space: 15.1 MB (91.41%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -1,18 +1,42 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
-        set<pair<int, int>> uniquePairs;
+        sort(nums.begin(), nums.end());
         int n = nums.size();
+        int cnt = 0;
 
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (abs(nums[i] - nums[j]) == k) {
-                    int a = min(nums[i], nums[j]);
-                    int b = max(nums[i], nums[j]);
-                    uniquePairs.insert({a, b});
+        if(n==1) return 0;
+
+        if(n == 2) {
+            int diff = abs(nums[0]-nums[1]);
+            return diff == k ? 1:0;
+        }
+
+       int prev1 = 1e9;
+       int prev2 = 1e9; 
+
+
+        for(int i = 0; i<n-1; i++) {
+            for(int j = i+1; j<n; j++) {
+                int diff = abs(nums[i] - nums[j]);
+                if(diff == k) {
+                    if(prev1 == 1e9 && prev2 == 1e9) {
+                        prev1 = nums[i];
+                        prev2 = nums[j];
+                        cnt++;
+                    }
+                    else if(prev1 != 1e9 && prev2 != 1e9) {
+                        if(prev1 == nums[i]) break;
+                        else {
+                            prev1 = nums[i];
+                            prev2 = nums[j];
+                            cnt++;
+                        } 
+                    }
                 }
+                else if(diff > k) break;
             }
         }
-        return uniquePairs.size();
+        return cnt;
     }
 };
