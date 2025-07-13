d6d0ff8b8cf3341b75614b23de059bee8e8a9e8f
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2025-07-13 20:51:05 +0530

    Time: 15 ms (75.58%), Space: 38.9 MB (23.39%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,41 +3,38 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
+
+
         vector<vector<int>> vis(m, vector<int>(n,0));
         vector<vector<int>> ans(m, vector<int>(n,0));
-        queue<pair<pair<int,int>,int>> q;
-        // adding intital 0s in queue
+
+        queue<pair<pair<int, int>, int>> q;
+
         for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(mat[i][j] == 0){
-                    q.push({{i,j}, 0});
-                    vis[i][j] = 1;
+            for(int j = 0; j<n; j++) {
+                if(mat[i][j] == 0) q.push({{i, j}, 0}), vis[i][j] = 1;
+            }
+        }
+        
+        int drow[] = {-1,0,1,0};
+        int dcol[] = {0,1,0,-1};
+        while(!q.empty()) {
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int d = q.front().second;
+            q.pop();
+            ans[r][c] = d;
+            for(int i = 0; i<4; i++) {
+                int nr = r + drow[i];
+                int nc = c + dcol[i];
+
+                if(nr>=0 && nr<m && nc>=0 && nc<n && !vis[nr][nc]){
+                    vis[nr][nc] = 1;
+                    q.push({{nr, nc}, d+1});
                 }
             }
         }
 
-        //
-        int drow[] = {-1,0,1,0};
-        int dcol[] = {0,1,0,-1};
-        while(!q.empty()){
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int dist = q.front().second;
-            q.pop();
-            ans[r][c] = dist;
-            for(int i = 0; i<4; i++){
-                int nr = r + drow[i];
-                int nc = c + dcol[i];
-
-                if(nr>=0 && nc>=0 && nr<m && nc<n && vis[nr][nc] == 0){
-                    vis[nr][nc] = 1;
-                    q.push({{nr,nc}, dist + 1});
-                }
-            
-            } 
-        }
-
         return ans;
-
     }
 };
