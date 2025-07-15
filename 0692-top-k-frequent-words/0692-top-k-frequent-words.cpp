fedf0fd266be4321605d67e3a7be08e76cba6c30
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2025-07-15 13:28:06 +0530

    Time: 6 ms (27.56%), Space: 17.2 MB (76.84%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -2,27 +2,29 @@ class Solution {
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
         unordered_map<string, int> mpp;
-        
-        // this i helps in remembering the occurance of a string bcz in vec when u inc the cnt u have to know the number so assigning it a number(first occurance index) is helpful
-        int i = 0;
-        vector<pair<int, string>> temp;
 
-        for(auto it:words) {
+        vector<pair<int, string>> temp;
+        int i = 0;
+        for(auto it:words){
             if(!mpp.count(it)){
                 mpp[it] = i++;
                 temp.push_back({1, it});
             }
-            temp[mpp[it]].first++;
+            else temp[mpp[it]].first++;
         }
 
-        sort(temp.begin(), temp.end(), [](auto& a, auto& b) {
-           if(a.first > b.first) return true;
-           else if(a.first == b.first && a.second < b.second) return true;
-           return false;
+        sort(temp.begin(),temp.end(), [](auto& a, auto& b){
+            if(a.first>b.first) return true;
+            else if(a.first == b.first && a.second < b.second) return true;
+            return false;
         });
 
-        vector<string> ans(k);
-        for(int i=0;i<k;i++)ans[i]=temp[i].second;
+        vector<string> ans;
+        for(int i = 0 ; i<k; i++) {
+            ans.push_back(temp[i].second);
+        }
+
         return ans;
+
     }
 };
