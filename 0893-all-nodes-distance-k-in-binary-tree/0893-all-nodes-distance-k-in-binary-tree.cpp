152afae26f2dd0cff956893387e31d48003bbec4
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2025-07-15 20:55:40 +0530

    Time: 3 ms (81.76%), Space: 16.5 MB (7.86%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,76 +9,37 @@
  */
 class Solution {
 public:
-    void markParents(TreeNode* root, unordered_map<TreeNode*, TreeNode*>& parent_track, TreeNode* target) 
-    {
-        queue<TreeNode*> q;
-        q.push(root);
+unordered_map<TreeNode*, TreeNode*> mpp;
+vector<int> ans;
+unordered_set<TreeNode*> st;
+    void helper(TreeNode* root, TreeNode* par){
+        if(!root) return;
 
-        while(!q.empty()) {
-            TreeNode* current = q.front();
-            q.pop();
+        mpp[root] = par;
 
-            if(current->left) 
-            {
-                parent_track[current->left] = current;
-                q.push(current->left);
-            }
-            if(current->right)
-            {
-                parent_track[current->right] = current;
-                q.push(current->right);
-            }
-
-        }
+        helper(root->left, root);
+        helper(root->right, root);
     }
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) 
-    {       ios_base::sync_with_stdio(false);
-            cin.tie(NULL);
-            cout.tie(NULL);
-            
-            unordered_map<TreeNode*, TreeNode*> parent_track;
-            markParents(root, parent_track, target);
+    void helper2(TreeNode* root, int k, int curr){
+        if(!root) return;
+        if(st.find(root) != st.end()) return;
+        if(curr == k) {
+            ans.push_back(root->val);
+            return;
+        }
+        st.insert(root);
+        helper2(mpp[root], k, curr+1);
+        helper2(root->left, k, curr+1);
+        helper2(root->right, k, curr+1);
+        
+    }
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+       
+       helper(root, nullptr);
 
-            unordered_map<TreeNode*, bool> visited;
-            queue<TreeNode*> q;
-            q.push(target);
-            visited[target] = true;
-            int curr_level = 0;
-
-            while( !q.empty() )
-            {
-                int size1 = q.size();
-
-                if( curr_level++ == k ) break;
-
-                for( int i = 0; i < size1; i++ ) 
-                {
-                    TreeNode* current = q.front();q.pop();
-                    if( current->left && !visited[current->left] )  
-                    {
-                        q.push(current->left);
-                        visited[current->left] = true;
-                    }
-                    if( current->right && !visited[current->right] ) 
-                    {
-                        q.push(current->right);
-                        visited[current->right] = true;
-                    }
-
-                    if( parent_track[current] && !visited[parent_track[current]] ) 
-                    {
-                        q.push(parent_track[current]);
-                        visited[parent_track[current]] = true;
-                    }
-                }
-            }
-
-            vector<int> res;
-            while( !q.empty() )
-            {
-                TreeNode* current = q.front(); q.pop();
-                res.push_back(current->val);
-            }
-            return res;
+    
+        helper2(target, k, 0);
+        return ans;
+        
     }
 };
