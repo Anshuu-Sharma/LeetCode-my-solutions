19bedfb5b7256ab4c433f32ac59aec56bbbced0a
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2025-08-06 19:23:44 +0530

    Time: 5 ms (81.99%), Space: 22.9 MB (73.49%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,42 +1,20 @@
 class Solution {
 public:
-    long long findHRS(vector<int> piles, int mid, int n){
-        long long total = 0;
-        for(int i = 0;i<n;i++){
-            total = total + ceil((double)piles[i]/(double)mid);
-        }
-        return total;
-    }
-    int maxEl(vector<int> piles, int n){
-        int maxi=piles[0];
-        for(int i = 0;i<n;i++){
-            if(maxi < piles[i]){
-                maxi = piles[i];
-            }
-        }
-        return maxi;
-    }
     int minEatingSpeed(vector<int>& piles, int h) {
-         ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-        int n = piles.size();
-        int low = 1, high = maxEl(piles,n);
-        int ans;
-        while( low <= high ){
-            int mid = ( low + high ) / 2;
-           long long total = findHRS(piles, mid, n);
-            if(total <= h){
-                high = mid - 1;
+        int ans = 0;
+        int low = 1, high = *max_element(piles.begin(), piles.end());
+        while(low<=high){
+            int mid = low + (high - low) / 2;
+            long long totalHours = 0;
+            for(auto it: piles){
+                totalHours += (it + mid - 1)/mid;
+            }
+            if(totalHours<=h){
+                high = mid-1;
                 ans = mid;
             }
-            else {
-                low = mid + 1;
-            }
-      }
-      return ans;
-
-
-
+            else low = mid+1;
+        }   
+        return ans;
     }
 };
