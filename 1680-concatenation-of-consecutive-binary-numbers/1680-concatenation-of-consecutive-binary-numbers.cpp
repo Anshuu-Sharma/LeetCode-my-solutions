b6d35b1350007ad6df12ee8b4c9e0359271347ae
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-02-28 12:01:21 +0530

    Time: 41 ms (61.75%), Space: 8.1 MB (69.8%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -3,10 +3,14 @@ public:
 int M = 1e9+7;
     int concatenatedBinary(int n) {
         long s = 0;
-
+        int digits = 0;
         for(int i = 1; i<=n; i++){
-            int x = log2(i) + 1;
-            s = ((s << x)%M + i)%M; 
+            // int x = log2(i) + 1;
+            // use the concept of power of 2 to find the number of bits to shift the number
+            // When you write int i = 5;, you see the decimal number 5. However, in your computer's memory, it is already stored as 101.
+            if((i & (i-1)) == 0) digits++;
+
+            s = ((s << digits)%M + i)%M; 
         }
 
         return s;
