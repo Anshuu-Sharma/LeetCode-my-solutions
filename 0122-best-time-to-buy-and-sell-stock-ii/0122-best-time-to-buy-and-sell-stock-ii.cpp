d529b9738f38b962e29f591e2530a386f4d0bb4d
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-03-22 18:57:18 +0530

    Time: 15 ms (10.71%), Space: 27.9 MB (5.49%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,67 +1,31 @@
 class Solution {
 public:
+    int helper(int n, int idx, int buy, vector<int>& prices, vector<vector<int>>& dp) {
+        if(idx == n) return 0;
+
+        if(dp[idx][buy] != -1) return dp[idx][buy];
+
+        int profit = 0;
+        if(buy){
+            profit = max(
+                -prices[idx] + helper(n, idx+1, 0, prices, dp),
+                0 + helper(n, idx+1, 1, prices, dp)
+            );
+        } 
+
+        else{
+            profit = max(
+                prices[idx] + helper(n, idx + 1, 1, prices, dp),
+                0 + helper(n, idx + 1, 0, prices, dp)
+            );
+        }
+
+        return dp[idx][buy] = profit;
+    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        // vector<vector<int>> dp(n, vector<int>(2,-1));
-        // return recursionHelper(prices, 0, 1, n, dp);
-        vector<vector<int>> dp(n+1, vector<int>(2,-1));
-        dp[n][0] = dp[n][1] = 0;
 
-        for(int ind = n-1; ind>=0; ind--){
-            for(int buy = 0; buy<=1; buy++){
-                long profit = 0;
-
-                if(buy){
-            if(dp[ind][buy] != -1){
-                return dp[ind][buy];
-            }
-            else{
-             profit = max(-prices[ind] + dp[ind+1][0],
-                        0 + dp[ind+1][buy]);
-            }
-        } 
-        else{
-            if(dp[ind][buy] != -1){
-                return dp[ind][buy];
-            }
-            else{
-             profit = max(prices[ind] + dp[ind+1][1],
-            0 + dp[ind+1][0]);
-            }
-        }
-            dp[ind][buy] = profit;
-            }
-        }
-        return dp[0][1];
-
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return helper(n, 0, 1, prices, dp);
     }
-
-
-
-
-    // int recursionHelper(vector<int>& prices, int ind, int buy, int n,vector<vector<int>>& dp){
-    //     if(ind == n) return 0;
-
-    //     if(buy){
-    //         if(dp[ind][buy] != -1){
-    //             return dp[ind][buy];
-    //         }
-    //         else{
-    //          profit = max(-prices[ind] + recursionHelper(prices, ind+1,0,n,dp),
-    //                     0 + recursionHelper(prices, ind+1,1, n,dp));
-    //         }
-    //     } 
-    //     else{
-    //         if(dp[ind][buy] != -1){
-    //             return dp[ind][buy];
-    //         }
-    //         else{
-    //          profit = max(prices[ind] + recursionHelper(prices, ind+1, 1, n,dp),
-    //         0 + recursionHelper(prices, ind+1,0,n,dp));
-    //         }
-    //     }
-    //     return profit;
-    // }
-
-    
 };
