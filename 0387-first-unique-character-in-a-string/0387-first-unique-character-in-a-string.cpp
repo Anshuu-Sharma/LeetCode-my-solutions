ce972aff60e38af5b18e0c2f059afe12805e462d
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-04-06 16:56:47 +0530

    Time: 0 ms (100%), Space: 14.7 MB (53.34%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public: 
+    int firstUniqChar(string s) {
+        vector<int> count(26,0);
+        int n = s.length();
+        // build char count bucket : <charIndex, count>
+        for (int i = 0; i < n; i++) {            
+            int index = s[i] - 'a';
+            count[index]++;
+        }
+        
+        // find the index
+        for (int i = 0; i < n; i++) {
+            int index = s[i] - 'a';
+            if (count[index] == 1) {
+                return i;
+            }   
+        }
+        return -1;
+    }
+};
