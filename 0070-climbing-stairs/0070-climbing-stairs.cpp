fdaa1e54d6117003f9717fbb7ab1a91bf6b5c1be
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-04-30 22:38:15 +0530

    Time: 0 ms (100%), Space: 7.8 MB (85.39%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    // int helper(int n, vector<int>& dp) {
-    //     if(n==0 || n==1) return dp[n] = 1;
-
-    //     if(dp[n] != -1) return dp[n];
-
-    //     return climbStairs(n-1) + climbStairs(n-2);
-    // }
+    // Tabulation dp approach -> bottom-up apporach -> space optimized apporach
     int climbStairs(int n) {
-        // if(n==0 || n==1) return 1;
+        // vector<int> dp(n+1, 0);
+        if(n==1) return 1;
 
-        // int oneStep = climbStairs(n-1);
-        // int twoStep = climbStairs(n-2);
-        // return oneStep+twoStep;
-
-        vector<int> dp(n+1,-1);
-        dp[0] = 1, dp[1] = 1;
-        for(int i = 2; i<=n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+        int prev_1 = 1;
+        int prev_2 = 1;
+        int curr = 0;
+        for(int i = 2; i<=n; i++){
+            curr = prev_1 + prev_2;
+            prev_1 = prev_2;
+            prev_2 = curr;
         }
-        return dp[n];
+
+        return curr;
     }
 };
