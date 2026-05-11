82f4f13fcab2d008aac34e7ceb7ed9e4eb160ca0
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-05-11 11:48:37 +0530

    Time: 543 ms (16.82%), Space: 296.7 MB (5.55%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int helper(int ind, int prev_ind, vector<int>& nums, vector<vector<int>>& dp){
-        if(ind==nums.size()) return 0;
+    int helper(int idx, int n, vector<int>& nums, int prev_idx, vector<vector<int>>& dp){
+        if(idx == n) return 0;
 
-        if(dp[ind][prev_ind+1] != -1) return dp[ind][prev_ind+1];
-    
-        int len = 0 + helper(ind+1, prev_ind, nums, dp);
-        if(prev_ind == -1 || nums[ind] > nums[prev_ind]) {
-            len = max(len, 1 + helper(ind+1, ind, nums, dp)); 
-        }
-        return dp[ind][prev_ind+1] = len;
-    }    
+        if(dp[idx][prev_idx+1] != -1) return dp[idx][prev_idx+1];
+
+        int take = 0;
+        int notTake = 0 + helper(idx+1, n, nums, prev_idx, dp);
+        if(prev_idx == -1 || nums[idx] > nums[prev_idx]) take = 1 + helper(idx+1, n, nums, idx, dp);
+
+        return dp[idx][prev_idx+1] = max(take, notTake);
+    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
+        
+        // prev_idx has range -1 to n-1 and -1 cannot be accomodated in array therefore we shift this by 1 so that it becomes 0 to n but idx is only 0 to n-1 because it doesnt have -1 as its value.
         vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return helper(0,-1, nums, dp);
+        return helper(0, n, nums, -1, dp);
     }
 };
