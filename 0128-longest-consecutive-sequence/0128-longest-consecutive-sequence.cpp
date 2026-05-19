03de990f5fc6652161f06030efed44f2178fa6f2
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-05-19 21:03:32 +0530

    Time: 107 ms (14.94%), Space: 93.6 MB (5.98%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    
+    class DisjointSet {
+    public:
+        vector<int> parent, size;
+
+        DisjointSet(int n) {
+            parent.resize(n);
+            size.resize(n, 1);
+
+            for(int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        int findUPar(int node) {
+            if(node == parent[node]) return node;
+
+            return parent[node] = findUPar(parent[node]);
+        }
+
+        void unionBySize(int u, int v) {
+            int pu = findUPar(u);
+            int pv = findUPar(v);
+
+            if(pu == pv) return;
+
+            if(size[pu] < size[pv]) {
+                parent[pu] = pv;
+                size[pv] += size[pu];
+            }
+            else {
+                parent[pv] = pu;
+                size[pu] += size[pv];
+            }
+        }
+    };
+
+    int longestConsecutive(vector<int>& nums) {
+
+        int n = nums.size();
+
+        DisjointSet ds(n);
+
+        unordered_map<int, int> mpp;
+
+        for(int i = 0; i < n; i++) {
+
+            // skip duplicates
+            if(mpp.find(nums[i]) != mpp.end()) continue;
+
+            // check num - 1
+            if(mpp.find(nums[i] - 1) != mpp.end()) {
+                ds.unionBySize(i, mpp[nums[i] - 1]);
+            }
+
+            // check num + 1
+            if(mpp.find(nums[i] + 1) != mpp.end()) {
+                ds.unionBySize(i, mpp[nums[i] + 1]);
+            }
+
+            mpp[nums[i]] = i;
+        }
+
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(ds.findUPar(i) == i) {
+                ans = max(ans, ds.size[i]);
+            }
+        }
+
+        return ans;
+    }
+};
