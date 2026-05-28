d6cacff0f40d666a40ea038821bb510fecd18453
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-05-29 01:25:47 +0530

    Time: 183 ms (15.43%), Space: 110.6 MB (23.42%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
+    bool helper(int ind, int n, int target, vector<int>& nums,  vector<vector<int>>& dp){
+        if(target == 0) return true;
+        if(ind == n-1) return false;
+
+        if(dp[ind][target] != -1) return dp[ind][target];
+
+        int take = false;
+        if(nums[ind] <= target) take = helper(ind+1, n, target - nums[ind], nums, dp);
+        int notTake = helper(ind+1, n, target, nums, dp);
+
+        return dp[ind][target] = take || notTake;
+    }
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
-        int sum = 0;
-         for(int i = 0; i<n; i++){
-            sum += nums[i];
-        }
-        if(sum%2) return false;
-        int target = sum/2;
+        int target = accumulate(nums.begin(), nums.end(), 0);
+        if(target & 1) return false;
+        target = target/2;
 
-        vector<vector<int>> dp(n, vector<int>(target+1, 0));
-        // base conditions
-        for(int i = 0; i<n; i++){
-            dp[i][0] = 1;
-        }
+        vector<vector<int>> dp(n, vector<int>(target+1, -1));
 
-        if(nums[0] <= target) dp[0][nums[0]] = 1;
-        
-        // Main loop
-        for(int i = 1; i<n; i++){
-            for(int j = 1; j<=target; j++){
-                // if(dp[i][j] != -1) continue;
-                
-            bool notTake = dp[i-1][j];
-            bool take = false;
-            if(nums[i] <= j){
-                take = dp[i-1][j - nums[i]];
-            }
-            
-            dp[i][j] = take || notTake;
-            }
-        }
-        return dp[n-1][target];
+        return helper(0, n, target, nums, dp);
     }
 };
