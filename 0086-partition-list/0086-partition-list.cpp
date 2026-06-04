c551f294d79427d46195d258056e4d6b85403bd5
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-06-04 15:30:13 +0530

    Time: 0 ms (100%), Space: 15 MB (42.17%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.cpp
@@ -0,0 +1,29 @@
+
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* small = new ListNode(0);
+        ListNode* large = new ListNode(0);
+
+        ListNode* smallp = small;
+        ListNode* largep = large;
+
+        while(head!=NULL){
+
+            if(head->val < x){
+                smallp->next = head;
+                smallp = smallp->next;
+            }else{
+                largep->next = head;
+                largep = largep->next;
+            }
+            head = head->next;
+        }
+
+        smallp->next = large->next;
+        largep->next = NULL;
+
+        return small->next;
+
+    } //small ek khali node h jisko smallp point kr rha h 
+};
