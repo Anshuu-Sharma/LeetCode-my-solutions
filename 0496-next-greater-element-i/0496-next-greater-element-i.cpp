cd8e0667cc0a69f89525efb997c6e3a1fcd43a99
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-06-05 18:00:28 +0530

    Time: 4 ms (23.02%), Space: 12.8 MB (36.74%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ls;
-        for(int i = 0; i<nums1.size(); i++) {
-            for(int j = 0; j<nums2.size(); j++) {
-                if(nums1[i] == nums2[j]) {
-                    int great = -1;
-                    for(int k = j; k<nums2.size(); k++) {
-                        if(nums2[k] > nums2[j]) {
-                            great = nums2[k];
-                            break;
-                        }         
-                    }
-                    ls.push_back(great);
-                    break;
-                }
+        // first find next greater element of nums 2
+        unordered_map<int, int> mpp;
+        stack<int> st;
+
+        int n2 = nums2.size();
+        for(int i = n2-1; i>=0; i--){
+            while(!st.empty() && st.top() < nums2[i]){
+                st.pop();
+            }
+            // If stack is empty, no greater element exists
+    if (st.empty()) {
+        mpp[nums2[i]] = -1;
+    } else {
+        // Otherwise, top is the next greater
+        mpp[nums2[i]] = st.top();
+    }
+            st.push(nums2[i]);
+        }
+        vector<int> ans;
+        for (int num : nums1) {
+            if (mpp.count(num)) {
+                ans.push_back(mpp[num]);
+            } else {
+                ans.push_back(-1);
             }
         }
-        return ls;
+        return ans;
+
     }
 };
