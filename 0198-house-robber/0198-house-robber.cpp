92aa8f05e53909a90a331652778bda3f8b7c59e4
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-06-16 13:41:42 +0530

    Time: 0 ms (100%), Space: 9.9 MB (97.31%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -15,17 +15,23 @@ public:
         int n = nums.size();
         if(n == 1) return nums[0];
 
-        vector<int> dp(n, 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        // vector<int> dp(n, 0);
+        // dp[0] = nums[0];
+        // dp[1] = max(nums[0], nums[1]);
+
+        int prev2 = nums[0];
+        int prev1 = max(nums[0], nums[1]);
 
         for(int i = 2; i<n; i++){
-            int take = nums[i] + dp[i-2];
-            int notTake = 0 + dp[i-1];
-            dp[i] = max(take, notTake);
+            int take = nums[i] + prev2;
+            int notTake = 0 + prev1;
+            // dp[i] = max(take, notTake);
+            int curr = max(take, notTake);
+            prev2 = prev1;
+            prev1 = curr;
         }
 
-        return dp[n-1];
+        return prev1;
         // return max(helper(0, n, nums, dp), helper(1, n, nums, dp));
     }
 };
