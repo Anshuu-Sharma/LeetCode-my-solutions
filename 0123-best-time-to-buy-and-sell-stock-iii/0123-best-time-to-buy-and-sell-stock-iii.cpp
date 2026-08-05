84f40dfebca0e95eb36d3272508781e8b50a95ad
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-08-05 15:01:16 +0530

    Time: 405 ms (42.78%), Space: 223.5 MB (31.86%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,46 +1,37 @@
 class Solution {
 public:
-    // int recursionHelper(vector<int>& prices, int ind, int buy, int n, int cap, vector<vector<vector<int>>> dp){
-    //     if(ind == n || cap ==0) return 0;
+    // int helper(int ind, int buy, int cap, vector<int>& prices, vector<vector<vector<int>>>& dp){
+    //     if(ind == prices.size() || cap == 0) return 0;
 
-    //         if(dp[ind][buy][cap] != -1) return dp[ind][buy][cap];
+    //     if(dp[buy][ind][cap] != -1) return dp[buy][ind][cap];
+
     //     if(buy){
-    //         return max(-prices[ind] + recursionHelper(prices, ind+1, 0, n, cap, dp), 0 + recursionHelper(prices, ind+1, 1, n, cap, dp));
+    //         return dp[buy][ind][cap] = max(-prices[ind] + helper(ind+1, 0, cap, prices, dp), 0 + helper(ind+1, 1, cap, prices, dp));
     //     }
-    //     return max(prices[ind] + recursionHelper(prices, ind + 1, 1, n, cap-1, dp), 0 + recursionHelper(prices, ind+1, 0, n, cap, dp));
+    //     else{
+    //         return dp[buy][ind][cap] = max(prices[ind] + helper(ind+1, 1, cap-1, prices, dp), 0 + helper(ind+1, 0, cap, prices, dp));
+    //     }
     // }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int>(3,-1)));
-        // return recursionHelper(prices, 0, 1, n, 2,dp);
+        int cap = 2;
+        vector<vector<vector<int>>> dp(2, vector<vector<int>>(n+1, vector<int>(cap+1, 0)));
 
-        //base case1
-        for(int ind = 0; ind<n; ind++){
-            for(int buy = 0; buy<=1; buy++){
-                dp[ind][buy][0] = 0;
-            }
-        }
-        //base case2
-        for(int buy = 0; buy<2; buy++){
-            for(int cap = 0; cap<3; cap++){
-                dp[n][buy][cap] = 0;
-            }
-        }
+        // base case already set to 0. so no need to set them exclusively
 
-        //main code
         for(int ind = n-1; ind>=0; ind--){
-            for(int buy = 0; buy<2; buy++){
-                for(int cap = 1; cap<=2; cap++){
+            for(int buy = 0; buy<=1; buy++){
+                for(int k = 1; k<=cap; k++){
                      if(buy){
-           dp[ind][buy][cap] = max(-prices[ind] + dp[ind+1][0][cap], 0 + dp[ind+1][1][cap]);
-        }
-        else{
-        dp[ind][buy][cap] = max(prices[ind] + dp[ind+1][1][cap-1], 0 + dp[ind+1][0][cap]);
-        }
+                        dp[buy][ind][k] = max(-prices[ind] + dp[!buy][ind+1][k], 0 + dp[buy][ind+1][k]);
+                    }
+                    else{
+                        dp[buy][ind][k] = max(prices[ind] + dp[!buy][ind+1][k-1], 0 + dp[buy][ind+1][k]);
+                    }
                 }
             }
         }
-        return dp[0][1][2];
-    }
 
+        return dp[1][0][2];
+    }
 };
