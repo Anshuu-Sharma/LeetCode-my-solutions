d40d69bc8fc1e0d3fb5e866658100cf60d1f3a8b
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-08-14 13:15:32 +0530

    Time: 4 ms (7.85%), Space: 17.3 MB (93.12%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,29 +1,48 @@
-class Solution {
-public: 
+//Union Find
+class UnionFind {
+private:
+    vector<int> parent;
 
-    bool dfs(int node, int col, vector<int>& color,vector<vector<int>>& graph){
-        color[node] = col;
-        for(auto it:graph[node]){
-            if(color[it] == -1){
-                if(dfs(it,!col,color,graph) == false){
-                    return false;
-                }
-            }
-            else if(color[it] == col) return false;
-        }
-        return true;
+public:
+    UnionFind(int size) {
+        parent.resize(size);
+        for (int i = 0; i < size; ++i)
+            parent[i] = i;
     }
-    bool isBipartite(vector<vector<int>>& graph) {
-        int v = graph.size();
-        vector<int> color(v,-1);
 
-        for(int i = 0; i<v; i++){
-            if(color[i] == -1){
-                if(dfs(i,0,color,graph) == false){
-                    return false;
-                }
+    int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]); // Path compression
+        return parent[x];
+    }
+
+    void unite(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        if (px != py)
+            parent[px] = py;
+    }
+};
+
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        UnionFind uf(n);
+
+        for (int i = 0; i < n; ++i) {
+            if (graph[i].empty()) continue;
+
+            // All neighbors should belong to the same group, different from node i
+            for (int j = 1; j < graph[i].size(); ++j) {
+                uf.unite(graph[i][0], graph[i][j]);
             }
+
+            // If i and its neighbor belong to same group → Not bipartite
+            if (uf.find(i) == uf.find(graph[i][0]))
+                return false;
         }
+
         return true;
     }
 };
