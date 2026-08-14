4d5a5a80c168bcbdca944f1986b326325d076e2d
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-08-14 13:32:52 +0530

    Time: 2 ms (75.67%), Space: 18.3 MB (70.93%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,53 +1,34 @@
 class Solution {
-private:
-    vector<int> ans;
-    bool hasCycle = false;
-
 public:
-    void dfs(int node, vector<int>& vis, vector<vector<int>>& adj) {
-        if (hasCycle) return;
-        
-        vis[node] = 1; // Mark as 'currently visiting' in the current path
-
-        for (auto it : adj[node]) {
-            if (vis[it] == 0) {
-                dfs(it, vis, adj);
-            } else if (vis[it] == 1) {
-                // If we hit a node that is currently in our DFS path, a cycle exists
-                hasCycle = true; 
-                return;
-            }
-        }
-
-        vis[node] = 2; // Mark as 'fully visited' and safe
-        ans.push_back(node);
-    }
-
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        // 0 = unvisited, 1 = visiting, 2 = completely visited
-        vector<int> vis(numCourses, 0);
+        vector<int> inDegree(numCourses, 0);
         vector<vector<int>> adj(numCourses);
-        ans.clear();
-
-        // Build adjacency list
-        for (int i = 0; i < prerequisites.size(); i++) {
-            int u = prerequisites[i][1];
-            int v = prerequisites[i][0];
-            adj[u].push_back(v); // u must be taken before v
+        
+        // Build graph & calculate in-degrees
+        for (auto& p : prerequisites) {
+            adj[p[1]].push_back(p[0]); // p[1] -> p[0]
+            inDegree[p[0]]++;
         }
-
-        // Run DFS on all unvisited nodes
-        for (int i = 0; i < numCourses; i++) {
-            if (vis[i] == 0) {
-                dfs(i, vis, adj);
+        
+        queue<int> q;
+        for (int i = 0; i < numCourses; ++i) {
+            if (inDegree[i] == 0) q.push(i);
+        }
+        
+        vector<int> topo;
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            topo.push_back(u);
+            
+            for (int v : adj[u]) {
+                if (--inDegree[v] == 0) {
+                    q.push(v);
+                }
             }
         }
-
-        // If a cycle is detected, no valid schedule exists
-        if (hasCycle) return {}; 
-
-        // DFS gives reverse topological sort, so we reverse it
-        reverse(ans.begin(), ans.end());
-        return ans;
+        
+        // If topo vector contains all courses, no cycle exists!
+        return topo.size() == numCourses ? topo : vector<int>();
     }
 };
