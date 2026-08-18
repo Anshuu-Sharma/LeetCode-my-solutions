77d84d8991f96f2106a73b7b0925ad852fdafef6
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-08-18 14:36:59 +0530

    Time: 3 ms (61.18%), Space: 24 MB (5.08%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,38 +1,43 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// 2nd method, 1st was using priority queue
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<int, vector<int>, greater<int>> minHeap;
-
-        for(auto it : lists) {
-            ListNode* temp = it;
-            while(temp) {
-                minHeap.push(temp->val);
-                temp=temp->next;
+        if (lists.empty()) {
+            return nullptr;
+        }
+        return mergeKListsHelper(lists, 0, lists.size() - 1);
+    }
+    
+    ListNode* mergeKListsHelper(vector<ListNode*>& lists, int start, int end) {
+        if (start == end) {
+            return lists[start];
+        }
+        if (start + 1 == end) {
+            return merge(lists[start], lists[end]);
+        }
+        int mid = start + (end - start) / 2;
+        ListNode* left = mergeKListsHelper(lists, start, mid);
+        ListNode* right = mergeKListsHelper(lists, mid + 1, end);
+        return merge(left, right);
+    }
+    
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* curr = dummy;
+        
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                curr->next = l1;
+                l1 = l1->next;
+            } else {
+                curr->next = l2;
+                l2 = l2->next;
             }
+            curr = curr->next;
         }
-
-        if(minHeap.empty()) return nullptr;
         
-        ListNode* ans = new ListNode(minHeap.top());
-        ListNode* root = ans;
-        minHeap.pop();
+        curr->next = l1 ? l1 : l2;
         
-        while(!minHeap.empty()) {
-            ListNode*  temp = new ListNode(minHeap.top());
-            minHeap.pop();
-            ans->next = temp;
-            ans = ans->next;
-        }
-        return root;
+        return dummy->next;
     }
 };
