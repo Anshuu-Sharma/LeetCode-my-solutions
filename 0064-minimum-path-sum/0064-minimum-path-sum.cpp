a6d443405eb0ccb904a71b7f8454c08ceeb066cf
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-08-22 21:48:17 +0530

    Time: 0 ms (100%), Space: 16.2 MB (62.66%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-    // long long recursionHelper(vector<vector<int>>& grid, int i, int j,vector<vector<int>>& dp){
-    //     if(i==0 && j==0) return grid[0][0];
-    //     if(i<0 || j<0) return INT_MAX;
+    int minSum = INT_MAX;
+    int recursion(int i, int j, int m, int n, vector<vector<int>>& grid){
+        if(i == m-1 && j == n-1) {
+            return grid[i][j];
+        }
+        int down = INT_MAX;
+        int right = INT_MAX;
+        if(i+1<m) down = grid[i][j] + recursion(i+1, j, m,n, grid);
+        if(j+1<n) right = grid[i][j] + recursion(i, j+1, m, n, grid);
 
-    // if(dp[i][j] != -1) return dp[i][j];
-    //     long long up = grid[i][j] + recursionHelper(grid, i-1, j,dp);
-    //     long long left = grid[i][j] + recursionHelper(grid, i, j-1, dp);
+        return min(down, right);
+    }
+    int tabulation(vector<vector<int>>& grid){
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[m-1][n-1] = grid[m-1][n-1];
 
-    //     return dp[i][j] = min(up,left);
-
-    // }
+        for(int i = m-1; i>=0; i--){
+            for(int j = n-1; j>=0; j--){
+                if(i == m-1 && j == n-1) continue;
+                int down = INT_MAX;
+                int right = INT_MAX;
+                if(i+1<m) down = grid[i][j] + dp[i+1][j];
+                if(j+1<n) right = grid[i][j] + dp[i][j+1];
+                dp[i][j] = min(down, right);
+            }
+        }
+        return dp[0][0];
+    }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<int> prev(n, 0);
-        vector<int> curr(n, 0);
-
-        // vector<vector<int>> dp(m, vector<int>(n, -1));
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(i == 0 && j==0) curr[j] = grid[0][0];
-                else{
-                    int up = grid[i][j];
-                    if(i>0) up += prev[j];
-                    else up += 1e9;
-
-                    int left = grid[i][j];
-                    if(j>0) left += curr[j-1];
-                    else left+=1e9;
-               curr[j] = min(up, left);
-                }
-            }
-            prev =curr;
-        }
-        return prev[n-1];
+        // return recursion(0,0,m,n,grid);
+        return tabulation(grid);
     }
 };
