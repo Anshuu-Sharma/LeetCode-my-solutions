2a630528126c266ec75ee666e9caec3ef49f3aff
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-08-22 10:11:57 +0530

    Time: 74 ms (16.29%), Space: 21.7 MB (19.82%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    int helper(int ind, vector<int>& nums, vector<int>& memo){
-        if(ind >= nums.size() - 1) return 0; // Reached the end
-        if(memo[ind] != -1) return memo[ind]; // Return cached result
-        
-        int ans = 1e9;
-        
-        // Jump forward based on the CURRENT index's capacity
-        for(int i = 1; i <= nums[ind]; i++){
-            ans = min(ans, 1 + helper(ind + i, nums, memo));
+    int recursion(int ind, int n, vector<int>& nums){
+        if(ind >= n-1) return 0;
+
+        int jump = 1e9;
+        for(int i = 1; i<=nums[ind]; i++){
+            jump = min(jump, 1 + recursion(ind+i, n, nums));
         }
         
-        return memo[ind] = ans; // Save to cache
+        return jump;
     }
-    
+    int tabulation(vector<int>& nums){
+        int n = nums.size();
+
+        vector<int> dp(n,1e9);
+        dp[n-1] = 0;
+        for(int i = n-2; i>=0; i--){
+            for(int j = 1; j<=nums[i]; j++){
+                if(i+j<n) dp[i] = min(dp[i], 1 + dp[i+j]);
+            }
+        }
+
+        return dp[0];
+    }
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> memo(nums.size(), 1e9);
-
-        memo[n-1] = 0;
-        for(int i = n-2; i>=0; i--){
-            for(int j = 1; j <= nums[i]; j++){
-                if(i+j < n) memo[i] = min(memo[i], 1 + memo[i+j]);
-            }
-        }   
-
-        return memo[0];
+        // return recursion(0,n,nums);
+        return tabulation(nums);
     }
 };
