a4895e93f172d98b88227ad7a9bbbdcf258cf29b
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-09-02 13:28:33 +0530

    Time: 0 ms (100%), Space: 77.2 MB (80%) - LeetHub

---
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+// method 2 - dfs
+    void reverse(TreeNode* left, TreeNode* right, int level){
+        if(left==NULL) return;
+
+        if(level&1) swap(left->val,right->val);
+
+        reverse(left->left, right->right, level+1);
+        reverse(left->right, right->left, level+1);
+    }
+
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        reverse(root->left,root->right,1);
+        return root;
+    }
+};
