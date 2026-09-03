484e22244a2309540f6f241ef75765506b2d0da1
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-09-03 16:55:33 +0530

    Time: 0 ms (100%), Space: 22.2 MB (41.06%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> st;
-        int i = 0;
-        int n = asteroids.size();
-        while (i < n) {
-            int item = asteroids[i];
-            bool isDestroyed = false; // checks if item is destroyed;
-            if (st.empty()) {
-                st.push(item);
-            } else {
-                if (!((item ^ st.top()) >= 0)) {
-                    while (!st.empty() && item < 0 && st.top() > 0) {
-                        if (abs(st.top()) < abs(item)) {
-                            st.pop();
-                            continue;
-                        } else if (abs(st.top()) == abs(item)) {
-                            st.pop();
-                            isDestroyed = true;
-                            break;
-                        } else {
-                            // top asteroid is larger; current destroyed
-                            isDestroyed = true;
-                            break; // current asteroid destroyed, exit loop
-                        }
-                    }
+        stack<int> stack;
+        
+        for (int asteroid : asteroids) {
+            bool alive = true;
+            while (!stack.empty() && asteroid < 0 && stack.top() > 0) {
+                if (stack.top() < -asteroid) {
+                    stack.pop();
+                    continue;
+                } else if (stack.top() == -asteroid) {
+                    stack.pop();
                 }
-                if (isDestroyed == false)
-                    st.push(item);
+                alive = false;
+                break;
             }
-            i++;
+            if (alive) {
+                stack.push(asteroid);
+            }
         }
-        vector<int> ans;
-        while (!st.empty()) {
-            ans.push_back(st.top());
-            st.pop();
+        
+        vector<int> result(stack.size());
+        for (int i = stack.size() - 1; i >= 0; --i) {
+            result[i] = stack.top();
+            stack.pop();
         }
-        ranges::reverse(ans);
-        return ans;
+        
+        return result;
     }
 };
