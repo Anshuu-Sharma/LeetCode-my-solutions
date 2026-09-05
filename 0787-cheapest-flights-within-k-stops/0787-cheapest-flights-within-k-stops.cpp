f4576743fcac7a55faef2df1355ad3425cffca5a
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-09-05 11:15:46 +0530

    Time: 0 ms (100%), Space: 17.8 MB (73.62%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> adj(n);
-        for(auto it:flights) {
-            adj[it[0]].push_back({it[1], it[2]});
+        vector<vector<pair<int,int>>> adj(n);
+        // {{1, 100}, ...}
+
+        for(int i = 0; i<flights.size(); i++){
+            adj[flights[i][0]].push_back({flights[i][1], flights[i][2]});
         }
 
-        // stops, src, distance from source
-        queue<pair<int, pair<int, int>>> q;
-        q.push({0,{src,0}});
+
         vector<int> dist(n, 1e9);
+        dist[src] = 0;
+        queue<pair<int, pair<int, int>>> q; // {stops, {node, dist}}
+        q.push({0, {src, 0}});
 
-        while(!q.empty()) {
-            auto it = q.front();q.pop();
-            int stops = it.first;
+        while(!q.empty()){
+            auto it = q.front(); q.pop();
             int node = it.second.first;
-            int dis = it.second.second;
-            if(stops>k) break;
+            int stops = it.first;
+            int distance = it.second.second;
 
-            for(auto itr : adj[node]) {
-                int adjNode = itr.first;
-                int adjWeight = itr.second;
-                
-                if(dis + adjWeight < dist[adjNode]) {
-                    dist[adjNode] = dis + adjWeight;
-                    q.push({stops+1, {adjNode, dis + adjWeight}});
+
+            for(auto it:adj[node]){
+                int newNode = it.first;
+                int newWeight = it.second;
+
+                if(distance + newWeight < dist[newNode] && stops<=k){
+                    dist[newNode] = distance + newWeight;
+                    q.push({stops+1,{newNode, dist[newNode]}});
                 }
             }
         }
-        if(dist[dst] == 1e9) return -1;
-        return dist[dst];
+
+        return dist[dst] == 1e9 ? -1 : dist[dst];
     }
 };
