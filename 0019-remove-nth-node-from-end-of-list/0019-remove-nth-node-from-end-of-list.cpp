dbddde41fcd7d5a66c9175187b539b995f7a5171
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-09-06 19:45:27 +0530

    Time: 0 ms (100%), Space: 15.1 MB (26.04%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,23 +11,26 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
+        /*
+        To find the nth node from the end without knowing the total length of the list, maintain a gap of $n$ nodes between two pointers (fast and slow). When fast reaches the end of the list, slow will be positioned right before the node that needs to be deleted.
+        */
         ListNode* slow = head;
         ListNode* fast = head;
-        for(int i = 0; i<n; i++) {
+
+        for(int i = 0; i<n; i++){
             fast = fast->next;
         }
 
-        if(fast == NULL) return head->next;
-        while (fast->next != NULL) {
-            fast = fast->next;
+        if(fast == nullptr) return head->next; // if n = length of LL then remove the head
+        
+        while(fast->next){
             slow = slow->next;
+            fast = fast->next;
         }
 
         ListNode* delNode = slow->next;
         slow->next = slow->next->next;
+
         delete delNode;
         return head;
     }
