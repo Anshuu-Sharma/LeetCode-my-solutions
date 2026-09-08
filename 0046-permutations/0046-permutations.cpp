001bcf54016efe580b390a4f520902c06c284852
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-09-08 23:13:36 +0530

    Time: 0 ms (100%), Space: 10.5 MB (88.79%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,20 +1,40 @@
+// class Solution {
+// public:
+//     int fact(int i){
+//         if (i <= 1) return 1;
+//         else return i*fact(i-1);
+//     }
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         int n = fact(nums.size());
+
+//         vector<vector<int>> ans;
+
+//         vector<int> temp = nums;
+//         for(int i = 0; i<n; i++){
+//             next_permutation(temp.begin(), temp.end());
+//             ans.push_back(temp);
+//         }
+
+//         return ans;
+//     }
+// };
 class Solution {
 public:
-    int fact(int i){
-        if (i <= 1) return 1;
-        else return i*fact(i-1);
-    }
-    vector<vector<int>> permute(vector<int>& nums) {
-        int n = fact(nums.size());
-        vector<vector<int>> ans;
-        vector<int> dummy = nums;
-        for(int i = 0; i<n; i++){
-        vector<int> temp = dummy;
-        next_permutation(temp.begin(), temp.end());
-            ans.push_back(temp);
-            dummy = temp;
+    void perms(int i,vector<vector<int>>& res,vector<int>& nums){
+        if(i==nums.size()){
+            res.push_back(nums);
+            return;
         }
 
-        return ans;
+        for(int j=i;j<nums.size();j++){
+            swap(nums[i],nums[j]);
+            perms(i+1,res,nums);
+            swap(nums[i],nums[j]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        perms(0,res,nums);
+        return res ;
     }
 };
