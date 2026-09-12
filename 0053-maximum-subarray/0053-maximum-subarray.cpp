41767da608e1e90d3fcdc1d46eab383854c21b46
Ansh Sharma <165352253+Anshuu-Sharma@users.noreply.github.com>
2026-09-12 15:09:13 +0530

    Time: 0 ms (100%), Space: 74.5 MB (14.92%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
-    // sliding window
-    int maxSubArray(vector<int>& nums) {
-        int n = nums.size(), r = 0;
-        int sum = 0;
-        int ans = INT_MIN;
-        while(r<n){
-            if(sum<0){
-                sum = 0;
-            }
-            sum += nums[r++];
-            ans = max(ans, sum);
+    int maxSum = INT_MIN;
+    void recursion(int ind, int sum, vector<int>& nums){
+        if(ind == nums.size()) return;
+
+        int temp = sum + nums[ind];
+        maxSum = max(maxSum, temp);
+        if(temp<0) temp = 0;
+        recursion(ind+1, temp, nums);
+    }
+    int tabulation(vector<int>& nums){
+        int n = nums.size();
+        vector<int> dp(n);
+
+        dp[n-1] = nums[n-1]; 
+        int maxSum = dp[n-1]; 
+
+        for(int i = n-2; i>=0; i--){
+            dp[i] = max(nums[i], dp[i+1] + nums[i]);
+
+            maxSum = max(maxSum, dp[i]);
         }
-        return ans;
+        return maxSum;
+    }
+    int maxSubArray(vector<int>& nums) {
+        recursion(0, 0, nums);
+        return tabulation(nums);
     }
 };
